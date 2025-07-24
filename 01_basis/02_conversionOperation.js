83d828fdb97e0dc4cc71cef1dbe76586568f86b1
Ashrajleo18 <36545625+Ashrajleo18@users.noreply.github.com>
2025-07-24 07:30:03 +0000

    conversion in JS

---
+++ 01_basis/02_conversionOperation.js
@@ -0,0 +1,35 @@
+// score in strin format 
+let score = "55"
+//console.log(typeof score) 
+
+// Score converted to number format 
+
+let newScore = Number(score);
+//console.log(typeof newScore)
+//console.log(newScore);
+
+let score2 = null
+let newScore2 = Number(score2)
+console.log(typeof newScore2)
+console.log(newScore2)
+
+//if we conver to number 
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+let islogedin = "ashok"
+let booleanlogedin = Boolean (islogedin)
+console.log(typeof booleanlogedin)
+console.log(booleanlogedin)
+
+//  console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "ASHOK" => true
+
+let someNumber = 33
+let someStringNumber = String(someNumber)
+console.log(typeof someStringNumber)
+console.log(someStringNumber)
