87cd63e8083e81db2d54472a3c44d5869f8bce19
Ashrajleo18 <36545625+Ashrajleo18@users.noreply.github.com>
2025-07-24 08:59:11 +0000

    compleated

--- 01_basis/02_conversionOperation.js
+++ 01_basis/02_conversionOperation.js
@@ -10,8 +10,8 @@ let newScore = Number(score);
 
 let score2 = null
 let newScore2 = Number(score2)
-console.log(typeof newScore2)
-console.log(newScore2)
+// console.log(typeof newScore2)
+// console.log(newScore2)
 
 //if we conver to number 
 // "33" => 33
@@ -20,8 +20,8 @@ console.log(newScore2)
 
 let islogedin = "ashok"
 let booleanlogedin = Boolean (islogedin)
-console.log(typeof booleanlogedin)
-console.log(booleanlogedin)
+// console.log(typeof booleanlogedin)
+// console.log(booleanlogedin)
 
 //  console.log(booleanIsLoggedIn);
 
@@ -31,5 +31,26 @@ console.log(booleanlogedin)
 
 let someNumber = 33
 let someStringNumber = String(someNumber)
-console.log(typeof someStringNumber)
-console.log(someStringNumber)
+// console.log(typeof someStringNumber)
+// console.log(someStringNumber)
+
+// *********************** Operations ***********************
+
+// console.log(1 + "2"); // retrun 12
+// console.log("1" + 2); // retrun 12
+// console.log("1" + "2"); // retrun 12
+// console.log(1+3 + "3") // retrun 43
+// console.log("1"+2+2)// retrun 122
+// console.log((3+4)*5-16) // return 19 
+
+let counter = 110
+counter++
+console.log(counter) // increment value = 111
+
+let x = 3;
+const y = x++;
+console.log("X =",x,"y", y ) // // x is 4; y is 3
+
+let a = 3;
+const b = ++a;
+console.log("a =",a,"b", b ) // x is 4; y is 4
