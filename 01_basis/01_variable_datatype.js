8757f88336a7d83252cc8f9f7ba45f69743ebd2b
Ashrajleo18 <36545625+Ashrajleo18@users.noreply.github.com>
2025-07-24 06:53:59 +0000

    rename and create

---
+++ 01_basis/01_variable_datatype.js
@@ -0,0 +1,20 @@
+
+const email = "ashrajleo@gmail.com"
+const fname = "Ashraj"
+const age = 44
+let area;
+
+console.table([email,fname, age, area]);
+console.log("type of null is = ",typeof null)
+console.log("type of undefined is = ",typeof undefined)
+
+// null is the object 
+// undefnied is undefined
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
