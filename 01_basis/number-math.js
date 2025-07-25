791793297c5463ead6cce57727e08d077dcef9a3
Ashrajleo18 <36545625+Ashrajleo18@users.noreply.github.com>
2025-07-25 03:30:06 +0000

    save

---
+++ 01_basis/number-math.js
@@ -0,0 +1,8 @@
+let num = 100
+console.log(num.toString());
+
+let newnumber = num
+
+//let newnumber = num.toFixed(); // 
+console.log("newnumber =",typeof newnumber)
+console.log("num =",typeof num)
