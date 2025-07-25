285304f47e48c9f39bd34bfb13a32ffea87baa36
Ashrajleo18 <36545625+Ashrajleo18@users.noreply.github.com>
2025-07-25 05:42:15 +0000

    ok

--- 01_basis/number-math.js
+++ 01_basis/number-math.js
@@ -6,3 +6,13 @@ let newnumber = num
 //let newnumber = num.toFixed(); // 
 console.log("newnumber =",typeof newnumber)
 console.log("num =",typeof num)
+
+// How to get number in between range
+
+min = 10;
+max = 20;
+
+let newRandno = Math.floor((Math.random()* (max - min + 1 ))+min);
+console.log(newRandno);
+
+
